aa98fa5c7aba518f75789ab3aae05511341ba5c4
hugo12345678999 <vendpromax@gmail.com>
2024-09-04 18:07:46 -0300

    Initial commit

--- src/components/Main/SearchPagamentos/WhatsappApp.js
+++ src/components/Main/SearchPagamentos/WhatsappApp.js
@@ -2,12 +2,14 @@ import React, { useContext } from "react";
 import "./EditPagamento.css";
 import { Button } from "antd";
 import { AuthContext } from "../../../contexts/AuthContext";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import * as links from "../../../utils/links";
 
 const WhatsappApp = () => {
-  const navigate = useNavigate();
-  const { authInfo } = useContext(AuthContext);
+  const location = useLocation();
+  let navigate = useNavigate();
+  const { authInfo, setNotiMessage } = useContext(AuthContext);
+  const token = authInfo?.dataUser?.token;
   const { id } = useParams();
 
   const handleConfigurarWhatsapp = () => {
@@ -15,18 +17,11 @@ const WhatsappApp = () => {
     navigate(`${links.EDIT_WHATSAPP_MAQUINA}/${id}`);
   };
 
-
-
-  
   return (
     <div className="ConfigurarWhatsapp_container">
       <Button
-        className="ConfigurarWhatsapp_button"
-        onClick={() => {
-          navigate(`${links.FORNECEDOR_SEARCH_CANAIS}/${id}`, {
-            state: location.state,
-          });
-        }}
+      
+        onClick={handleConfigurarWhatsapp}
       >
         CONFIGURAR WHATSAPP
       </Button>
