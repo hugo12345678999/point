1e13f7b8839add17fd87b730fed8b54ff396337d
hugo12345678999 <vendpromax@gmail.com>
2024-09-07 10:24:23 -0300

    Initial commit

--- src/components/Main/SearchPagamentos/PagamentosSearch.js
+++ src/components/Main/SearchPagamentos/PagamentosSearch.js
@@ -63,7 +63,7 @@ const PagamentosSearch = (props) => {
   }, [dataFim]);
   useEffect(() => {
     if (estado == 2) {
-      navigate(`${links.WHATSAPP_MAQUINA}/${id}`); // Redireciona para PAGAMNETO_PPP com o ID na URL
+      navigate(`${links.TELEMETRIA_MAQUINA}/${id}`); // Redireciona para PAGAMNETO_PPP com o ID na URL
     }
   }, [estado, navigate, id, setNotiMessage]);
   useEffect(() => {
