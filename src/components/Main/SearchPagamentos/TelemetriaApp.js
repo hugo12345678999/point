df08443c3a760636008e286b054387b205c93457
hugo12345678999 <vendpromax@gmail.com>
2024-09-07 09:24:02 -0300

    Initial commit

--- src/components/Main/SearchPagamentos/TelemetriaApp.js
+++ src/components/Main/SearchPagamentos/TelemetriaApp.js
@@ -61,16 +61,7 @@ const TelemetriaApp = (props) => {
       getPaymentsPeriod(dataInicio, dataFim);
     }
   }, [dataFim]);
-  useEffect(() => {
-    if (estado == 2) {
-      navigate(`${links.WHATSAPP_MAQUINA}/${id}`); // Redireciona para PAGAMNETO_PPP com o ID na URL
-    }
-  }, [estado, navigate, id, setNotiMessage]);
-  useEffect(() => {
-    if (estado == 2) {
-      navigate(`${links.TELEMETRIA_MAQUINA}/${id}`); // Redireciona para PAGAMNETO_PPP com o ID na URL
-    }
-  }, [estado, navigate, id, setNotiMessage]);
+
 
   const getData = (id) => {
     if (id.trim() !== "") {
