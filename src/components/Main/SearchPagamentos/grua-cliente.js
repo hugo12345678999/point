928941b12916c0f3338821755a8c3af268830b43
hugo12345678999 <vendpromax@gmail.com>
2024-09-16 20:07:47 -0300

    Initial commit

--- src/components/Main/SearchPagamentos/grua-cliente.js
+++ src/components/Main/SearchPagamentos/grua-cliente.js
@@ -13,60 +13,62 @@ const GruaCliente = (props) => {
 
   return (
     <div className="GruaCliente_container">
-      <div className="GruaCliente_header">
-        <Link className="GruaCliente_header_back" to={links.DASHBOARD_FORNECEDOR}>
-          VOLTAR
-        </Link>
-      </div>
-      <div className="GruaCliente_menu">
-        <div 
-          className="GruaCliente_menu_item"
-          onClick={() => navigate(`${links.EDIT_PROBABILIDADE_CLIENTE}/${id}`, { state: location.state })}
-        >
-          <AiOutlineEdit />
-          <span>Probabilidade</span>
+      <div className="GruaCliente_square">
+        <div className="GruaCliente_header">
+          <Link className="GruaCliente_header_back" to={links.DASHBOARD_FORNECEDOR}>
+            VOLTAR
+          </Link>
         </div>
-        <div 
-          className="GruaCliente_menu_item"
-          onClick={() => navigate(`${links.EDIT_GARRA_FORTE_CLIENTE}/${id}`, { state: location.state })}
-        >
-          <AiOutlineEdit />
-          <span>Garra Forte</span>
+        <div className="GruaCliente_menu">
+          <div 
+            className="GruaCliente_menu_item"
+            onClick={() => navigate(`${links.EDIT_PROBABILIDADE_CLIENTE}/${id}`, { state: location.state })}
+          >
+            <AiOutlineEdit />
+            <span>Probabilidade</span>
+          </div>
+          <div 
+            className="GruaCliente_menu_item"
+            onClick={() => navigate(`${links.EDIT_GARRA_FORTE_CLIENTE}/${id}`, { state: location.state })}
+          >
+            <AiOutlineEdit />
+            <span>Garra Forte</span>
+          </div>
+          <div 
+            className="GruaCliente_menu_item"
+            onClick={() => navigate(`${links.EDIT_GARRA_MEDIA_CLIENTE}/${id}`, { state: location.state })}
+          >
+            <AiOutlineEdit />
+            <span>Garra Média</span>
+          </div>
+          <div 
+            className="GruaCliente_menu_item"
+            onClick={() => navigate(`${links.EDIT_GARRA_FRACA_CLIENTE}/${id}`, { state: location.state })}
+          >
+            <AiOutlineEdit />
+            <span>Garra Fraca</span>
+          </div>
+          <div 
+            className="GruaCliente_menu_item"
+            onClick={() => navigate(`${links.EDIT_GARRA_PREMIO_CLIENTE}/${id}`, { state: location.state })}
+          >
+            <AiOutlineEdit />
+            <span>Garra Prêmio</span>
+          </div>
+          <div 
+            className="GruaCliente_menu_item"
+            onClick={() => navigate(`${links.ALTERAR_GRUA_CLIENTE}/${id}`, { state: location.state })}
+          >
+            <AiOutlineEdit />
+            <span>Alterar Grua</span>
+          </div>
         </div>
-        <div 
-          className="GruaCliente_menu_item"
-          onClick={() => navigate(`${links.EDIT_GARRA_MEDIA_CLIENTE}/${id}`, { state: location.state })}
-        >
-          <AiOutlineEdit />
-          <span>Garra Média</span>
+        <div className="GruaCliente_buttons">
+          <Button className="GruaCliente_button_edit" onClick={() => navigate(`${links.ALTERAR_GRUA_CLIENTE}/${id}`, { state: location.state })}>
+            <AiOutlineEdit />
+            Alterar Configuração
+          </Button>
         </div>
-        <div 
-          className="GruaCliente_menu_item"
-          onClick={() => navigate(`${links.EDIT_GARRA_FRACA_CLIENTE}/${id}`, { state: location.state })}
-        >
-          <AiOutlineEdit />
-          <span>Garra Fraca</span>
-        </div>
-        <div 
-          className="GruaCliente_menu_item"
-          onClick={() => navigate(`${links.EDIT_GARRA_PREMIO_CLIENTE}/${id}`, { state: location.state })}
-        >
-          <AiOutlineEdit />
-          <span>Garra Prêmio</span>
-        </div>
-        <div 
-          className="GruaCliente_menu_item"
-          onClick={() => navigate(`${links.ALTERAR_GRUA_CLIENTE}/${id}`, { state: location.state })}
-        >
-          <AiOutlineEdit />
-          <span>Alterar Grua</span>
-        </div>
-      </div>
-      <div className="GruaCliente_buttons">
-        <Button className="GruaCliente_button_edit" onClick={() => navigate(`${links.ALTERAR_GRUA_CLIENTE}/${id}`, { state: location.state })}>
-          <AiOutlineEdit />
-          Alterar Configuração
-        </Button>
       </div>
     </div>
   );
