81ef3cb03ff19afb0474e11b0e35e9526c49cb60
hugo12345678999 <vendpromax@gmail.com>
2024-09-21 10:58:14 -0300

    Initial commit

--- src/components/Main/RemoteCredit/Reiniciar-maquina.js
+++ src/components/Main/RemoteCredit/Reiniciar-maquina.js
@@ -1,78 +1,20 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../../contexts/AuthContext";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Button, Input } from "antd";
+import { Button } from "antd";
 import LoadingAction from "../../../themes/LoadingAction/LoadingAction";
 import * as links from "../../../utils/links";
-import { FORNECEDOR_SEARCH_CANAIS } from "../../../utils/links";
 
-const Reiniciar = (props) => {
+const Reiniciar = () => {
     const { id } = useParams();
-    const location = useLocation();
-    const maquinaInfos = location.state;
-    const { setDataUser, loading, authInfo, setNotiMessage } = useContext(AuthContext);
-    let navigate = useNavigate();
+    const { setDataUser, authInfo, setNotiMessage } = useContext(AuthContext);
+    const navigate = useNavigate();
     const token = authInfo?.dataUser?.token;
     const [isLoading, setIsLoading] = useState(false);
-    const [data, setData] = useState({ valor: '' });
-    const [errors, setErrors] = useState({});
-    const [garrafraca, setGarrafraca] = useState(null); // Estado para armahzenar o valor da garra forte
-    const [matchMessage, setMatchMessage] = useState(""); // Mensagem de correspondência
-
-    const handleChange = (name, value) => {
-        setData(prev => ({
-            ...prev,
-            [name]: value
-        }));
-        setErrors(prev => {
-            let errorsTemp = { ...prev }
-            delete errorsTemp[name]
-            return errorsTemp
-        })
-    }
-
-    const fetchGarrafraca = () => {
-        axios.get(`${process.env.REACT_APP_SERVIDOR}/garra-fraca/${id}`, {
-            headers: {
-                "x-access-token": token,
-                "content-type": "application/json",
-            },
-        })
-        .then((res) => {
-            setGarrafraca(res.data.garrafraca); // Atualize o estado com o valor da garra forte recebido
-        })
-        .catch((err) => {
-            // Lide com os erros da requisição
-        });
-    };
 
     const onSave = () => {
-        // check require
-        let errorsTemp = {}
-
-        if(data.valor.trim() < 1){
-            errorsTemp.valor = 'DIGITE UM VALOR MAIOR OU IGUAL A 1'
-       
-        }else{
-            setNotiMessage({
-                type: 'success',
-                message: 'CONFIGURANDO GARRA FRACA, AGUARDE 3 MINUTOS PARA CONFIGURAR OUTRA FUNCAO!'
-            })
-
-        }
-
-        if (Object.keys(errorsTemp).length > 0) {
-            setErrors(errorsTemp)
-            return;
-        }
-        let VALOR_BASE;
-        if(data.valor < 10){
-            VALOR_BASE = 800;
-        }else{
-            VALOR_BASE = 80;
-        }
-        setIsLoading(true)
+        setIsLoading(true);
         axios.post(`${process.env.REACT_APP_SERVIDOR}/credito-remoto-cliente`, {
             id: id,
             valor: 4002,
@@ -82,87 +24,40 @@ const Reiniciar = (props) => {
                 "content-type": "application/json",
             }
         })
-        .then(res => {
-            setIsLoading(false)
-            setData({
-                valor: ''
-            })
-            if (parseFloat(data.valor) === garrafraca) {
+        .then(() => {
+            setIsLoading(false);
+            setNotiMessage({
+                type: 'success',
+                message: 'CONFIGURANDO GARRA FRACA! AGUARDE O VALOR SER MODIFICADO PARA CONFIGURAR OUTRA FUNÇÃO!'
+            });
+        })
+        .catch(err => {
+            setIsLoading(false);
+            if ([401, 403].includes(err.response?.status)) {
                 setNotiMessage({
                     type: 'error',
-                    message: 'O VALOR É IGUAL'
+                    message: 'A sua sessão expirou, para continuar faça login novamente.'
                 });
+                setDataUser(null);
             } else {
                 setNotiMessage({
-                    type: 'success',
-                    message: `CONFIGURANDO GARRA FRACA! AGUARDE O VALOR SER MODIFICADO PARA CONFIGURAR OUTRA FUNCAO!`
+                    type: 'error',
+                    message: `Erro, algo deu errado ${err.response?.data?.msg}`
                 });
             }
-        })
-            .catch(err => {
-                setIsLoading(false)
-                if ([401, 403].includes(err.response.status)) {
-                    // setNotiMessage('A sua sessão expirou, para continuar faça login novamente.');
-                    setNotiMessage({
-                        type: 'error',
-                        message: 'A sua sessão expirou, para continuar faça login novamente.'
-                    })
-                    setDataUser(null);
-                } else {
-                    setNotiMessage({
-                        type: 'error',
-                        // message: 'Erro, algo deu errado ' + (err.response?.data?.msg ?? "")
-                        message: `Erro, algo deu errado ${err.response?.data?.msg}`
-                    })
-                }
-            })
-    }
-
-    const checkMatch = () => {
-        if (parseFloat(data.valor) === garrafraca) {
-            setMatchMessage("O VALOR ÌGUAL!");
-        } else {
-            setMatchMessage("");
-        }
+        });
     };
-
-    useEffect(() => {
-        fetchGarrafraca(); // Fetch da garra forte ao montar o componente
-        const interval = setInterval(fetchGarrafraca, 10000); // Atualiza a cada 10 segundos
-        return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
-    }, [id]); // Execute sempre que o ID mudar
-
-    useEffect(() => {
-        checkMatch(); // Verifique a correspondência ao montar o componente
-    }, [data.valor, garrafraca]); // Execute sempre que o valor do campo de entrada ou a garra forte mudar
 
     return (
         <>
             {isLoading && <LoadingAction />}
-            <div className="AddMachine_container">
-                <div className="AddMachine_header">
-                    <div className="AddMachine_header_title">
-                        GARRA FRACA {maquinaInfos.nome}
-                    </div>
-                    <div className="AddMachine_header_back" onClick={() => {
-                        navigate(`${links.FORNECEDOR_SEARCH_CANAIS}/${id}`, { state: location.state });
-                    }}>
-                        VOLTAR
-                    </div>
-                </div>
-                <div className="AddMachine_content">
-                    
-                    <Button className="AddMachine_saveBtn" onClick={() => {
-                        if (!isLoading) onSave()
-                    }} disabled={isLoading}>
-                        REINICIAR
-                    </Button>
-                   
-                </div>
-
+            <div style={{ textAlign: "center", marginTop: "20px" }}>
+                <Button onClick={onSave} disabled={isLoading}>
+                    REINICIAR
+                </Button>
             </div>
         </>
-    )
+    );
 }
 
 export default Reiniciar;
